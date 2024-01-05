864eb665c43938a04f060a26dfa854dd95501416
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-01-05 22:05:01 +0530

    Formatting ProductsPage

--- src/main/java/com/qa/pages/ProductsPage.java
+++ src/main/java/com/qa/pages/ProductsPage.java
@@ -5,8 +5,6 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 
 public class ProductsPage extends BaseTest {
-
-
     @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"test-Cart drop zone\"]/android.view.ViewGroup/android.widget.TextView")
     private WebElement products_info;
 
