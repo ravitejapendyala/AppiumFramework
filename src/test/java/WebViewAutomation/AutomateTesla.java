cef43e54df4b32e7e2fe551b8f7e5465a3d4d615
ravitejapendyala <VenkataRaviTeja.Pendyala@blueyonder.com>
2024-04-25 20:25:04 +0530

    Update appium test for tesla

--- src/test/java/WebViewAutomation/AutomateTesla.java
+++ src/test/java/WebViewAutomation/AutomateTesla.java
@@ -35,7 +35,7 @@ public class AutomateTesla {
 
         element = driver.findElement(AppiumBy.xpath("//h2[text()='Charging']"));
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
-        Thread.sleep(4000);
+        Thread.sleep(5000);
 
         driver.findElement(AppiumBy.xpath("//span[text()='Continue']")).click();
 
